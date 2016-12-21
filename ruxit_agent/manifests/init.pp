26a8609a429358a60da746f892715d42e165dd2e
Peter Studener <peter.studener@dynatrace.com>
2016-12-21 10:40:38 +0100

    Support for Oneagent

--- ruxit_agent/manifests/init.pp
+++ ruxit_agent/manifests/init.pp
@@ -1,8 +1,8 @@
 # Class: ruxit_agent
 #
 # This module installs the ruxit agent on the host.
-# You can define which update channel you want to use and as well define a specific version you want 
-# to install. If no channel/ version is given as parameter this module will install the latest stable 
+# You can define which update channel you want to use and as well define a specific version you want
+# to install. If no channel/ version is given as parameter this module will install the latest stable
 # ruxit agent version.
 #
 #   Parameters:
@@ -26,26 +26,26 @@ class ruxit_agent (
   $version          = '',
   $channel          = 'stable',
   $log_keep_days    = '14',
-  $service_restarts = []
-  ) {
+  $service_restarts = []) {
   if $ruxit_server == '' {
     $ruxit_host = "https://${ruxit_tenant}.live.ruxit.com"
   } else {
     $ruxit_host = $ruxit_server
   }
+
   # os specific definitions
   if $::kernel =~ /Linux/ {
-    $usr_home       = "/home/${user}"
-    $install_dir    = "${usr_home}/ruxit-agent"
-    $installer_pre  = 'ruxit-Agent-Linux-'
-    $installer_ext  = '.sh'
+    $usr_home = "/home/${user}"
+    $install_dir = "${usr_home}/ruxit-agent"
+    $installer_pre = 'ruxit-Agent-Linux-'
+    $installer_ext = '.sh'
     $install_script = 'install.sh'
 
   } elsif $::kernel =~ /windows/ {
-    $usr_home       = "C:\\Users\\${user}"
-    $install_dir    = "${usr_home}\\ruxit-agent"
-    $installer_pre  = 'ruxit-Agent-Windows-'
-    $installer_ext  = '.msi'
+    $usr_home = "C:\\Users\\${user}"
+    $install_dir = "${usr_home}\\ruxit-agent"
+    $installer_pre = 'ruxit-Agent-Windows-'
+    $installer_ext = '.msi'
     $install_script = 'install.bat'
   }
 
@@ -53,22 +53,22 @@ class ruxit_agent (
   case $channel {
     /dev/      : {
       # use the development channel
-      $src            = 'http://download-dev.ruxitlabs.com/agent/unstable'
+      $src = 'http://download-dev.ruxitlabs.com/agent/unstable'
       $latest_version = $::ruxit_latest_version_dev
     }
     /sprint/   : {
       # use the sprint channel
-      $src            = 'http://download-sprint.ruxitlabs.com/agent/unstable'
+      $src = 'http://download-sprint.ruxitlabs.com/agent/unstable'
       $latest_version = $::ruxit_latest_version_sprint
     }
     /unstable/ : {
       # use the release unstable channel
-      $src            = 'http://download.ruxit.com/agent/unstable'
+      $src = 'http://download.ruxit.com/agent/unstable'
       $latest_version = $::ruxit_latest_version_unstable
     }
     default    : {
       # use the release stable channel as default
-      $src            = 'http://download.ruxit.com/agent'
+      $src = 'http://download.ruxit.com/agent'
       $latest_version = $::ruxit_latest_version_stable
     }
   }
@@ -76,13 +76,14 @@ class ruxit_agent (
     true    => $latest_version,
     default => $version
   }
-  $script_mode          = '0777'
+  $script_mode = '0777'
 
   # trigger log cleanup on every run -> has a schedule defined
   ruxit_agent::resources::cleanup_log { 'cleanup_ruxit_logs': days_to_keep => $log_keep_days }
 
   # Only do the install steps if there is no ruxit agent installed
-  if (!empty($ruxit_required_version) and $ruxit_required_version != '0.0.0.0' and $::ruxit_installed_version == '0.0.0.0') {
+  if (!empty($ruxit_required_version) and $ruxit_required_version != '0.0.0.0' and $::ruxit_installed_version == '0.0.0.0' and 
+  $::oneagent_installed_version == '0.0.0.0') {
     $installer = "${installer_pre}${ruxit_required_version}${installer_ext}"
 
     # create the download directory
@@ -109,10 +110,8 @@ class ruxit_agent (
       uri     => "${src}/${installer}",
       dest    => "${install_dir}/${installer}",
       require => File[$install_dir]
-    }
-    ->
-    ruxit_agent::resources::install_ruxit_agent { $ruxit_required_version: }
-    ->
+    } ->
+    ruxit_agent::resources::install_ruxit_agent { 'install_ruxit_agent': required_version => $ruxit_required_version } ->
     ruxit_agent::resources::restart_services_hook { $service_restarts: }
   }
   # end install
--- ruxit_agent/manifests/resources/install_ruxit_agent.pp
+++ ruxit_agent/manifests/resources/install_ruxit_agent.pp
@@ -1,10 +1,17 @@
 # install ruxit agent binary
-define ruxit_agent::resources::install_ruxit_agent {
-
+define ruxit_agent::resources::install_ruxit_agent ($required_version = '') {
   if $::kernel =~ /Linux/ {
     $install_dest_dir = '/opt/ruxit/agent'
   } else {
     $install_dest_dir = 'C:\\Program Files (x86)\\ruxit\\agent'
+  }
+
+  if versioncmp('1.100', $required_version) > 0 {
+    $servicename_linux = 'ruxitagent'
+    $servicename_windows = 'ruxit Agent'
+  } else {
+    $servicename_linux = 'oneagent'
+    $servicename_windows = 'one Agent'
   }
 
   exec { 'install_ruxit_agent':
@@ -12,21 +19,13 @@ define ruxit_agent::resources::install_ruxit_agent {
     cwd       => $ruxit_agent::install_dir,
     timeout   => 600,
     logoutput => true,
-    require   => [
-      File[$ruxit_agent::install_dir],
-      File[$ruxit_agent::install_script],
-      Download['ruxit_installer_download']
-      ]
+    require   => [File[$ruxit_agent::install_dir], File[$ruxit_agent::install_script], Download['ruxit_installer_download']]
   }
 
   # restart the agent
   if $::kernel =~ /Linux/ {
-    ruxit_agent::resources::linux_restartservice_initd{ 'ruxitagent' :
-      subscribe => Exec['install_ruxit_agent']
-    }
+    ruxit_agent::resources::linux_restartservice_initd { $servicename_linux: subscribe => Exec['install_ruxit_agent'] }
   } else {
-    ruxit_agent::resources::windows_restartservice{ 'ruxit Agent' :
-      subscribe => Exec['install_ruxit_agent']
-    }
+    ruxit_agent::resources::windows_restartservice { $servicename_windows: subscribe => Exec['install_ruxit_agent'] }
   }
 }
