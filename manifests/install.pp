e34740591d6e0e59c6808845b198dadecd59924c
luisescobar89 <luis.scobr@gmail.com>
2021-01-20 14:26:09 -0600

    move download to separate manifest

--- manifests/install.pp
+++ manifests/install.pp
@@ -4,61 +4,36 @@
 #
 class dynatraceoneagent::install {
 
-  if !defined('archive') {
-    class { 'archive':
-      seven_zip_provider => '',
-    }
-  }
-
   $created_dir              = $dynatraceoneagent::created_dir
   $download_dir             = $dynatraceoneagent::download_dir
   $filename                 = $dynatraceoneagent::filename
   $download_path            = $dynatraceoneagent::download_path
-  $proxy_server             = $dynatraceoneagent::proxy_server
-  $command                  = $dynatraceoneagent::command
-  $download_link            = $dynatraceoneagent::download_link
   $provider                 = $dynatraceoneagent::provider
   $oneagent_params_hash     = $dynatraceoneagent::oneagent_params_hash
   $reboot_system            = $dynatraceoneagent::reboot_system
   $service_name             = $dynatraceoneagent::service_name
+  $package_state            = $dynatraceoneagent::package_state
+  $oneagent_puppet_conf_dir = $dynatraceoneagent::oneagent_puppet_conf_dir
 
-  file{ $download_dir:
-    ensure => directory
-  }
-
-  archive{ $download_path:
-    ensure         => present,
-    extract        => false,
-    source         => $download_link,
-    path           => $download_path,
-    allow_insecure => true,
-    require        => File[$download_dir],
-    creates        => $created_dir,
-    proxy_server   => $proxy_server,
-  }
-
-  if ($::kernel == 'Linux') or ($::osfamily  == 'AIX') {
-    file{ $download_path:
-      ensure => present,
-      mode   => '0755',
-    }
-
+  if ($::kernel == 'Linux') or ($::osfamily  == 'AIX') and ($package_state != absent) {
     exec { 'install_oneagent':
-        command   => $command,
+        command   => $dynatraceoneagent::command,
         cwd       => $download_dir,
         timeout   => 6000,
         creates   => $created_dir,
         provider  => $provider,
         logoutput => on_failure,
+        before    => File[$oneagent_puppet_conf_dir],
     }
   }
 
   if ($::osfamily == 'Windows'){
     package { $service_name:
-      ensure          => present,
+      ensure          => $package_state,
       provider        => $provider,
       source          => $download_path,
       install_options => [$oneagent_params_hash, '--quiet'],
+      before          => File[$oneagent_puppet_conf_dir],
     }
   }
 
